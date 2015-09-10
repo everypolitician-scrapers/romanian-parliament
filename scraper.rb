2af5d3231cdba7366b36966b381d3df0fb32d9f1
Tony Bowden <tony@tmtm.com>
2015-09-10 07:48:16 +0100

    New pass at scraper
    Start again with a simpler version

---
+++ scraper.rb
@@ -0,0 +1,72 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def date_parse(str)
+  return if str.to_s.empty?
+  return Date.parse(str).to_s rescue binding.pry
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+
+  current = noko.css('.grup-parlamentar-list table').first
+  current.xpath('.//tr[td]').each do |tr|
+    tds = tr.css('td')
+
+    if tds.count == 5 # combined
+      area, area_id = nil, nil
+      date_col = 4
+    else
+      area, area_id = tds[2].text.split(/\s*\/\s*/, 2)
+      date_col = 5
+    end
+    link = URI.join url, tds[1].css('a/@href').text
+
+    data = { 
+      id: link.to_s[/idm=(\d+)/, 1],
+      name: tds[1].text.tidy,
+      faction: tds[4].text.tidy,
+      area: area,
+      area_id: area_id,
+      term: 2012,
+      start_date: date_parse(tds[date_col].text),
+      start_date_raw: date_from(tds[date_col].text),
+      source: link.to_s,
+    }.merge(scrape_person(link))
+    puts data
+    # ScraperWiki.save_sqlite([:id, :term], data)
+  end
+end
+
+def scrape_person(url)
+  noko = noko_for(url)
+  box = noko.css('.stiri-detalii')
+  data = { 
+    sort_name: box.css('h1').text.tidy,
+    image: box.css('.profile-pic-dep img/@src').text,
+    birth_date: date_parse(box.css('.profile-pic-dep').text.tidy),
+    email: box.css('span.mailInfo').text,
+    #TODO history of parliamentary groups
+  }
+  data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
+  data
+end
+
+scrape_list('http://www.cdep.ro/pls/parlam/structura2015.de?leg=2012&idl=2')
