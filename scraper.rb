3eea2512daacbb79222dca1460d5c545b6589d9a
Tony Bowden <tony@tmtm.com>
2017-05-01 18:05:04 +0100

    Show debug output if MORPH_DEBUG is set

--- scraper.rb
+++ scraper.rb
@@ -115,7 +115,7 @@ data = scrape(start => MembersPage).members.map do |mem|
   mem.to_h.merge(scrape(mem.source => MemberPage).to_h)
 end
 
-# puts data.map { |r| r.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h }
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
 ScraperWiki.save_sqlite(%i[id term], data)
